f6d9f4914b6a1f6f0b9455b01cb9a4e02b334e69
sbx_user1051 <sbx_user1051@169.254.50.253>
2023-07-24 15:00:16 +0000

    Mon Jul 24 15:00:16 UTC 2023

--- src/main/kotlin/solutions/SUM/SumSolution.kt
+++ src/main/kotlin/solutions/SUM/SumSolution.kt
@@ -2,6 +2,10 @@ package solutions.SUM
 
 object SumSolution {
     fun sum(x: Int, y: Int): Int {
-        TODO("Solution not implemented")
+        if (x < 0 || x > 100 || y < 0 || y > 100) {
+            throw IllegalArgumentException("Arguments need to be between 0-100")
+        }
+        return x + y
     }
 }
+
